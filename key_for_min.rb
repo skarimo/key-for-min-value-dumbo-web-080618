b235c5dc1a6468020c48ccba461a1c9222561a4b
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 16:01:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,20 @@
 
 def key_for_min_value(name_hash)
   current_key = []
+  current_value = []
   
   if name_hash.empty?
       return nil 
   else
     name_hash.each do |key, value|
-      
-      
+      if current_key.empty? && current_value.empty?
+        current_key = key 
+        current_value = value 
+      elsif current_value > value
+        current_key = key 
+        current_value = value
+      end
+    end
+  end
+  current_value
 end

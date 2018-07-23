e2a5ed16a69a418f172acf11c71cde6e852e61bf
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 15:55:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,8 @@
 def key_for_min_value(name_hash)
   current_key = []
   name_hash.each do |key, value|
-    if name_hash = nil 
-      return
+    if name_hash == nil 
+      return nil
+    elsif current_key == nil
+      
 end
